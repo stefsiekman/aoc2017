4db2b4ffcb5e93f2375051fae814a56adfe39588
Stef Siekman <s.siekman@gmail.com>
2018-12-04 14:02:24 +0100

    Solve day 5

---
+++ 5/second.py
@@ -0,0 +1,24 @@
+def solve(lines):
+    instr = [int(i) for i in lines]
+
+    steps = 0
+    ptr = 0
+
+    while True:
+        steps += 1
+
+        offs = instr[ptr]
+        if instr[ptr] >= 3:
+            instr[ptr] -= 1
+        else:
+            instr[ptr] += 1
+        ptr += offs
+
+        if ptr >= len(instr) or ptr < 0:
+            break
+
+    print(steps)
+
+
+with open("input.txt", "r") as file:
+    solve(file.read().splitlines())
