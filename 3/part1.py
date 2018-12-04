8d27e3bf55705985e5f047b049fc617c04f30e22
Stef Siekman <s.siekman@gmail.com>
2018-12-04 08:51:14 +0100

    Minimize the solution to day 3

--- 3/part1.py
+++ 3/part1.py
@@ -1,64 +1,41 @@
 visited = {}
 location = (0,0)
-direction = "r" # l u d
+direction = 0
 
 n = 289326
 
 def neighbours(l):
     return {
-        "r": (l[0] + 1, l[1]    ),
-        "u": (l[0]    , l[1] + 1),
-        "l": (l[0] - 1, l[1]    ),
-        "d": (l[0]    , l[1] - 1)
+        0: (l[0] + 1, l[1]    ),
+        1: (l[0]    , l[1] + 1),
+        2: (l[0] - 1, l[1]    ),
+        3: (l[0]    , l[1] - 1)
     }
 
 def sumAround(l):
-    locations = [
-        (l[0] - 1, l[1] - 1),
-        (l[0]    , l[1] - 1),
-        (l[0] + 1, l[1] - 1),
+    r = [-1,0,1]
+    s = sum([visited[(l[0]+i,l[1]+j)] for i in r for j in r if (i != 0 or j != 0) and (l[0]+i,l[1]+j) in visited])
 
-        (l[0] - 1, l[1]    ),
-        (l[0] + 1, l[1]    ),
-
-        (l[0] - 1, l[1] + 1),
-        (l[0]    , l[1] + 1),
-        (l[0] + 1, l[1] + 1),
-    ]
-
-    sum = 0
-    for loc in locations:
-        if loc in visited:
-            sum += visited[loc]
-
-    if sum > 0:
-        return sum
+    if s > 0:
+        return s
     else:
         return 1
 
 for i in range(2, n + 1):
     # Visit current
-    sum = sumAround(location)
-    if sum > n:
-        print(sum)
+    s = sumAround(location)
+    if s > n:
+        print(s)
         exit()
-    else:
-        print(sum)
-    visited[location] = sum
+    visited[location] = s
 
     # Travel in direction
     location = neighbours(location)[direction]
 
     # Can we turn left?
-    toLeft = {
-        "r": "u",
-        "u": "l",
-        "l": "d",
-        "d": "r"
-    }
-    locationToLeft = neighbours(location)[toLeft[direction]]
+    locationToLeft = neighbours(location)[(direction + 1) % 4]
 
     if not locationToLeft in visited:
-        direction = toLeft[direction]
+        direction = (direction + 1) % 4
 
 print(f"{n} is at {abs(location[0]) + abs(location[1])}")
